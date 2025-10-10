ecfad4eed4361c41e45edaa96ec12fd51bfe7b63
Kaio Fábio <kaio.fabio.sport@gmail.com>
2025-10-09 21:01:28 -0300

    Add files via upload

---
+++ aula_js/script.js
@@ -0,0 +1,23 @@
+let paragrafo = document.getElementById("paragrafo-01")
+
+let texto = "meu texto"
+let numero = 1234567
+let booleano = true
+let meuArray = [-1, 2, 0, 3.1, -5.8]
+
+if(numero > 0 || numero < 5){
+
+}
+
+if(paragrafo != null){
+    paragrafo.innerText = texto + "-" + numero + "-" + booleano;
+}
+let valor = 13
+if (valor > 0 && < 10){
+    valor = 11
+}
+else if (valor < 0 && valor < 12){
+    valor = 15
+} else {
+    valor = 0
+}
